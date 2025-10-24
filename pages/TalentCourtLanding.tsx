3473bb88485748eea03d049d35b22e18d8cf61f3
Nnamdi Okeke <onnamdialex@gmail.com>
2025-10-24 11:00:08 +0100

    :zap: Improve performance / landing page wip

---
+++ pages/TalentCourtLanding.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { Upload, Search, MessageCircle, Shield, Play } from "lucide-react";
+
+const TalentCourtLanding = () => {
+  const [activeTestimonial, setActiveTestimonial] = useState(0);
+  return <div>TalentCourtLanding</div>;
+};
+
+export default TalentCourtLanding;
