9c90a013b38eb19f7b2303dd69130117c9b8adb0
Nnamdi Okeke <onnamdialex@gmail.com>
2026-06-26 00:22:19 +0100

    fix mistake

--- lib/constants/sid.ts
+++ lib/constants/sid.ts
@@ -1,9 +1,9 @@
-export const VALID_PREFIX = ["ATH", "SCT"];
+export const VALID_PREFIX = ["ATH", "SCT"] as const;
 
-export const PREFIX_SEPARATOR = ["-"];
+export const PREFIX_SEPARATOR = "-";
 
-export const NUMERIC_LENGTH = 7;
+export const NUMERIC_LENGTH = 5;
 
 export const MINIMUM_NUM = 1;
 
-export const MAXIMUM_NUM = 7;
+export const MAXIMUM_NUM = Math.pow(10, NUMERIC_LENGTH) - 1;
