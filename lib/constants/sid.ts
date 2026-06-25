fb3ca816d90a28a002bc608c1a7eb4587e494fda
Nnamdi Okeke <onnamdialex@gmail.com>
2026-06-26 00:19:03 +0100

    :art: Constants for the sid

---
+++ lib/constants/sid.ts
@@ -0,0 +1,9 @@
+export const VALID_PREFIX = ["ATH", "SCT"];
+
+export const PREFIX_SEPARATOR = ["-"];
+
+export const NUMERIC_LENGTH = 7;
+
+export const MINIMUM_NUM = 1;
+
+export const MAXIMUM_NUM = 7;
