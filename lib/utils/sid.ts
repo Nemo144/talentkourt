17a35623099877e31169e1f1cf9d05f68efa4860
Nnamdi Okeke <onnamdialex@gmail.com>
2026-06-27 23:46:52 +0100

    :sparkles: Complete the function for the generation of the SID

--- lib/utils/sid.ts
+++ lib/utils/sid.ts
@@ -1,4 +1,11 @@
-import { MAXIMUM_NUM, MINIMUM_NUM, NUMERIC_LENGTH } from "../constants/sid";
+import {
+  MAXIMUM_NUM,
+  MINIMUM_NUM,
+  NUMERIC_LENGTH,
+  VALID_PREFIX,
+} from "../constants/sid";
+
+type SIdPrefix = (typeof VALID_PREFIX)[number];
 
 export const padNumericId = (num: number): string => {
   return num.toString().padStart(NUMERIC_LENGTH, "0");
@@ -8,4 +15,10 @@ export const generateNumericId = (): number => {
   return (
     Math.floor(Math.random() * (MAXIMUM_NUM - MINIMUM_NUM + 1)) + MINIMUM_NUM
   );
+};
+
+export const generateSId = (prefix: SIdPrefix): string => {
+  const id = generateNumericId();
+  const paddedId = padNumericId(id);
+  return `${prefix}-${paddedId}`;
 };
