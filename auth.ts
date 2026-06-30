9f6b3267a7ab3f0a6cd7b2e7b7b5033108c4fb1b
Nnamdi Okeke <onnamdialex@gmail.com>
2026-06-30 17:32:29 +0100

    :zap: Fully wired up the auth system and connected to db and schema

--- auth.ts
+++ auth.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -9,8 +11,25 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: { label: "Password", type: "password" },
       },
       authorize: async (credentials) => {
-        //
-        return null;
+        const email = credentials.email as string;
+        const password = credentials.password as string;
+
+        //to find user by email
+        const user = await prisma.user.findUnique({
+          where: {
+            email: email,
+          },
+        });
+
+        //return null if user is not found
+        if (!user) return null;
+
+        //compare the password against the stored hashed password
+        const isValid = await bcrypt.compare(password, user.passwordHash);
+
+        if (!isValid) return null;
+
+        return user;
       },
     }),
   ],
